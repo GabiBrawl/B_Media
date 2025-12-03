f15558afabcaf69913e2bcd16268d4fa0f1faf8e
GabiBrawl <gabiya219@gmail.com>
2025-12-03 13:09:34 +0000

    Added 26 IEM extra data with B_Media direct links and audio graphs.

--- js/extraData.js
+++ js/extraData.js
@@ -26,7 +26,7 @@ const extraData = {
     },
     "LETSHUOER D02": {
         "images": [
-            ""
+            "images/extraData/letshuoer_d02_graph.png"
         ],
         "tiktoks": [
             "https://www.tiktok.com/@axel_bostrom/video/7550684897644530974"
@@ -66,5 +66,199 @@ const extraData = {
         ],
         "tiktoks": [],
         "otherStuff": "Here could go some additional information about the Kiwi Ears Belle. This is a temporary placeholder."
+    },
+    "Apos x Community Rock Lobster IEMs": {
+        "images": [
+            "images/extraData/apos_x_community_rock_lobster_iems_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7578584267219799327"
+        ],
+        "otherStuff": "Here could go some additional information about the Apos x Community Rock Lobster IEMs. This is a temporary placeholder."
+    },
+    "Moondrop LAN II": {
+        "images": [
+            "images/extraData/moondrop_lan_ii_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7568889877891239199"
+        ],
+        "otherStuff": "Here could go some additional information about the Moondrop LAN II. This is a temporary placeholder."
+    },
+    "Truthear x Crinacle Zero Blue 2": {
+        "images": [
+            "images/extraData/truthear_x_crinacle_zero_blue_2_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7531100140661132575"
+        ],
+        "otherStuff": "Here could go some additional information about the Truthear x Crinacle Zero Blue 2. This is a temporary placeholder."
+    },
+    "Kefine Delci": {
+        "images": [
+            "images/extraData/kefine_delci_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7532239194094193951"
+        ],
+        "otherStuff": "Here could go some additional information about the Kefine Delci. This is a temporary placeholder."
+    },
+    "Dunu Titan S2": {
+        "images": [
+            "images/extraData/dunu_titan_s2_graph.png"
+        ],
+        "tiktoks": [],
+        "otherStuff": "Here could go some additional information about the Dunu Titan S2. This is a temporary placeholder."
+    },
+    "SIMGOT EW300": {
+        "images": [
+            "images/extraData/simgot_ew300_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7535897194184985887"
+        ],
+        "otherStuff": "Here could go some additional information about the SIMGOT EW300. This is a temporary placeholder."
+    },
+    "MOONDROP RAYS": {
+        "images": [
+            "images/extraData/moondrop_rays_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7535099339782245662"
+        ],
+        "otherStuff": "Here could go some additional information about the MOONDROP RAYS. This is a temporary placeholder."
+    },
+    "Moondrop Aria 2": {
+        "images": [
+            "images/extraData/moondrop_aria_2_graph.png"
+        ],
+        "tiktoks": [],
+        "otherStuff": "Here could go some additional information about the Moondrop Aria 2. This is a temporary placeholder."
+    },
+    "Save 10%   Fosi Audio IM4": {
+        "images": [
+            "images/extraData/save_10____fosi_audio_im4_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7562192147487690014"
+        ],
+        "otherStuff": "Here could go some additional information about the Save 10%   Fosi Audio IM4. This is a temporary placeholder."
+    },
+    "Kiwi Ears \u00c9tude": {
+        "images": [
+            "images/extraData/kiwi_ears__tude_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7565164462156582174"
+        ],
+        "otherStuff": "Here could go some additional information about the Kiwi Ears \u00c9tude. This is a temporary placeholder."
+    },
+    "SIMGOT EM6L": {
+        "images": [
+            "images/extraData/simgot_em6l_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7575558274355825951"
+        ],
+        "otherStuff": "Here could go some additional information about the SIMGOT EM6L. This is a temporary placeholder."
+    },
+    "7Hz x Crinacle: Divine": {
+        "images": [
+            "images/extraData/7hz_x_crinacle__divine_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7571477178219515167"
+        ],
+        "otherStuff": "Here could go some additional information about the 7Hz x Crinacle: Divine. This is a temporary placeholder."
+    },
+    "7Hz x Crinacle: Diablo": {
+        "images": [
+            "images/extraData/7hz_x_crinacle__diablo_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7571477178219515167"
+        ],
+        "otherStuff": "Here could go some additional information about the 7Hz x Crinacle: Diablo. This is a temporary placeholder."
+    },
+    "Crinear Daybreak": {
+        "images": [
+            "images/extraData/crinear_daybreak_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7555507380008586526"
+        ],
+        "otherStuff": "Here could go some additional information about the Crinear Daybreak. This is a temporary placeholder."
+    },
+    "LETSHUOER S12 Ultra": {
+        "images": [
+            "images/extraData/letshuoer_s12_ultra_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7563663091683052831"
+        ],
+        "otherStuff": "Here could go some additional information about the LETSHUOER S12 Ultra. This is a temporary placeholder."
+    },
+    "NICEHCK NX8": {
+        "images": [
+            "images/extraData/nicehck_nx8_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7538803807380196639"
+        ],
+        "otherStuff": "Here could go some additional information about the NICEHCK NX8. This is a temporary placeholder."
+    },
+    "7HZ Timeless II": {
+        "images": [
+            "images/extraData/7hz_timeless_ii_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7568136665559059743"
+        ],
+        "otherStuff": "Here could go some additional information about the 7HZ Timeless II. This is a temporary placeholder."
+    },
+    "ZiiGaat x Hangout.Audio: Odyssey 2": {
+        "images": [
+            "images/extraData/ziigaat_x_hangout_audio__odyssey_2_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7545552509696331038"
+        ],
+        "otherStuff": "Here could go some additional information about the ZiiGaat x Hangout.Audio: Odyssey 2. This is a temporary placeholder."
+    },
+    "NICEHCK NX8 SE": {
+        "images": [
+            "images/extraData/nicehck_nx8_se_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7538803807380196639"
+        ],
+        "otherStuff": "Here could go some additional information about the NICEHCK NX8 SE. This is a temporary placeholder."
+    },
+    "Kiwi Ears Septet": {
+        "images": [
+            "images/extraData/kiwi_ears_septet_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7547714768803548446"
+        ],
+        "otherStuff": "Here could go some additional information about the Kiwi Ears Septet. This is a temporary placeholder."
+    },
+    "Kiwi Ears Astral": {
+        "images": [
+            "images/extraData/kiwi_ears_astral_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7534345763887811871"
+        ],
+        "otherStuff": "Here could go some additional information about the Kiwi Ears Astral. This is a temporary placeholder."
+    },
+    "Punch Audio Martilo": {
+        "images": [
+            "images/extraData/punch_audio_martilo_graph.png"
+        ],
+        "tiktoks": [
+            "https://www.tiktok.com/@axel_bostrom/video/7533976170300329247"
+        ],
+        "otherStuff": "Here could go some additional information about the Punch Audio Martilo. This is a temporary placeholder."
     }
 };
